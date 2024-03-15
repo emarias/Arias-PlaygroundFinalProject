e1bf265bc1a96e59fa2070a449d0da910ce8b3ae
Emanuel Arias <emanuelariasalejandro@gmail.com>
2024-03-15 04:23:53 -0300

    Se completó el CRUD del modelo Autos

--- vehiculos/urls.py
+++ vehiculos/urls.py
@@ -4,4 +4,7 @@ from vehiculos import views
 urlpatterns = [
     path('autos/', views.AutosLista.as_view() , name= 'autos'),
     path('autos/pedir/', views.PedirAuto.as_view() , name= 'PedirAuto'),
+    path('autos/<int:pk>/', views.DetallesAuto.as_view() , name= 'DetallesAuto'),
+    path('autos/<int:pk>/editar/', views.EditarAuto.as_view() , name= 'EditarAuto'),
+    path('autos/<int:pk>/eliminar/', views.SacarAuto.as_view() , name= 'SacarAuto'),
 ]
--- vehiculos/views.py
+++ vehiculos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from vehiculos.models import Autos
 from django.urls import reverse_lazy
 
@@ -15,3 +16,19 @@ class PedirAuto(CreateView):
     template_name = "autos/conseguir_auto.html"
     fields = ['modelo', 'color', 'marca', 'fecha_de_fabricacion','descripcion']
     success_url = reverse_lazy('autos')
+    
+class SacarAuto(DeleteView):
+    model = Autos
+    template_name = "autos/sacar_auto.html"
+    success_url= reverse_lazy ('autos')
+
+class EditarAuto(UpdateView):
+    model = Autos
+    template_name = "autos/editar_auto.html"
+    success_url= reverse_lazy ('autos')
+    fields= ['modelo', 'color', 'marca', 'fecha_de_fabricacion','descripcion']
+    
+class DetallesAuto(DetailView):
+    model = Autos
+    template_name = "autos/detalles_auto.html"
+    
